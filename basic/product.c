26653aee9e5bb613c1706b2aeedc8c2f70cc915f
kismatssi88 <kismatdahal24@gmail.com>
2026-04-05 07:14:54 +0545

    Add files via upload

---
+++ basic/product.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a ,b , p;
+    printf("Enter any two numbers");
+    scanf("%d %d",a,b);
+    p =a*b;
+    printf("the product is %d",p);
+    return 0;
+}
---
+++ basic/volumecube.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+    {
+    int l,V;
+    printf("enter the length of a cube");
+    scanf("%d",&l);
+    V = l*l*l;
+    printf("the volume of cube is %d",V);
+    return 0;
+    }
