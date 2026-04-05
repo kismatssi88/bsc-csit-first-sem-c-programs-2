58177c8daa5014db8f08dae76930541fa3e0370e
kismatssi88 <kismatdahal24@gmail.com>
2026-04-05 11:00:30 +0545

    Add files via upload

---
+++ basic/greatestamong3.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a,b,c;
+    printf("enter any three nnumbers:");
+    scanf("%d %d %d",&a,&b,&c);
+    if(a>b && a>c)
+    {
+     printf("the greatest number is %d",a);
+    }
+    else if(b>a && b>c)
+    {
+    printf("the greatest number is %d",b);
+    }
+    else
+    {
+    printf("the greatest number is %d",c);
+    }
+    return 0;
+    
+}
