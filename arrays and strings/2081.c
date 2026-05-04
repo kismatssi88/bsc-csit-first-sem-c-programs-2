1158e7ff4969b21f4f4ef09d01100b2fa118b77c
kismat dahal <kismatdahal24@gmail.com>
2026-05-04 18:12:34 +0545

    Add files via upload

--- arrays and strings/2081.c
+++ arrays and strings/2081.c
@@ -1,23 +1,22 @@
 //5 person age
 #include<stdio.h>
-#include<conio.h>
 int main()
 {
     int age[5],i;
     int total=0;
     float average;
-    for (i=1;i<=5;i++)
+    for (i=0;i<=5;i++)
     {
         printf("enter age");
-        scanf("%d",age[i]);
+        scanf("%d",&age[i]);
     }
-    for(i=1;i<=5;i++)
+    for(i=0;i<=5;i++)
     {
      total=total + age[i];
     }
     average=total/5;
     printf("the avaerage is %f",average);
-    for(i=1;i<=5;i++)
+    for(i=0;i<=5;i++)
     {
         if(age[i]>=25 && age[i]<=30)
         {
